a600d82c8d91cf447606afa81e79908c63451125
AlejandroML1988 <amayorlazaro@gmail.com>
2022-11-17 21:10:30 +0100

    Menu seleccion simple

--- src/entornos/calculadora.java
+++ src/entornos/calculadora.java
@@ -1,10 +1,58 @@
+/*Proyecto para el módulo "ENTORNOS DE DESARROLLO". Se trata de una calculadora.
+ * Este proyecto lo hacemos de forma colaborativa usando un repositorio Git, que subimos
+ * al servidor Github.
+ * 
+ * @Authors: Javier Espejo, Alejandro Mayor, Luis Rodríguez.
+ * Fecha de creación: 17/11/2022
+ * Última actualización: 17/11/2022 @Alejandro
+ * 
+ * Menú de selección:
+ * 	El usuario puede elegir entre sumar, restar y multiplicar 2 números.
+ * 	Si la opción elegida es incorrecta, se avisa al usuario por pantalla y
+ * 	se le pide de nuevo elegir una opción.
+ * 
+ * El resultado de la operación elegida se muestra por pantalla
+ */
+
 package entornos;
+
+import java.util.Scanner;
 
 public class calculadora {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner teclado = new Scanner(System.in); //objeto teclado para leer la entrada por teclado
+		//variables
+		String operacion; //Operacion elegida por el usuario
+		int a; //Primer número introducido por el usuario
+		int b; //Segundo número introducido por el usuario
+		int resultado;
+		
+		
+		do {// do-while Seleccion de menu correcta
+			
+			System.out.println("Elija una operacion a realizar: ");
+			System.out.println("+ ---> Sumar");
+			System.out.println("- ---> Restar");
+			System.out.println("* ---> Multiplicar");
+			operacion = teclado.nextLine();
+			
+			//Mensaje de error si la opcion elegida es incorrecta
+			if ( !( operacion.equals("+") || operacion.equals("-") || operacion.equals("*")))
+				System.out.println("Error, la opcion seleccionada es incorrecta.");
+			
+		}
+		while( !(operacion.equals("+") || operacion.equals("-") || operacion.equals("*")));
+		//Fin do-while Seleccion de menu correcta
+		
+		//Yo utilizaría "swtich-case" para cada operación posible.
+		
+		//Hacer suma: Javier
+		
+		//Hacer resta: Alejandro
+		
+		//Hacer multiplicación: Luis
+		
 	}//main
 
 }//class
